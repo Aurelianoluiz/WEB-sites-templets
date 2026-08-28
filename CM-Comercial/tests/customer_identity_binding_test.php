ee566d8fa4e88b9f076bd50e56ad1a842efbb8c0
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-28 08:58:34 -0300

    fix(comercial): correct customer identity regression test

--- CM-Comercial/tests/customer_identity_binding_test.php
+++ CM-Comercial/tests/customer_identity_binding_test.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 $source = (string)file_get_contents(__DIR__ . '/../customer_financial_history.php');
 
 $checks = [
-    'uses_authenticated_user_session' => str_contains($source, "$_SESSION['user']['id']"),
-    'does_not_accept_request_customer_id' => !str_contains($source, "$_GET['customer_id']") && !str_contains($source, "$_POST['customer_id']"),
+    'uses_authenticated_user_session' => str_contains($source, '$_SESSION[\'user\'][\'id\']'),
+    'does_not_accept_request_customer_id' => !str_contains($source, '$_GET[\'customer_id\']') && !str_contains($source, '$_POST[\'customer_id\']'),
     'rejects_missing_identity' => str_contains($source, 'http_response_code(401)'),
 ];
 
