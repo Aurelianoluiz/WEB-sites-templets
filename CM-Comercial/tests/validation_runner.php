3ba02bda8bedbddf21d03be66bfc5c2c2310e4d0
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-28 15:25:42 -0300

    test: include payment order policy in validation runner

--- CM-Comercial/tests/validation_runner.php
+++ CM-Comercial/tests/validation_runner.php
@@ -9,6 +9,7 @@ $tests = [
     'payment_core_test.php',
     'payment_service_test.php',
     'payment_operations_test.php',
+    'payment_order_policy_test.php',
     'customer_financial_history_test.php',
     'customer_identity_binding_test.php',
     'csrf_test.php',
