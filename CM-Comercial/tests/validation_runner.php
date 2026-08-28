dc7799e82455257be3a1e49ed7ce7061b0826e73
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-28 17:08:58 -0300

    test: add webhook lifecycle regression to runner

--- CM-Comercial/tests/validation_runner.php
+++ CM-Comercial/tests/validation_runner.php
@@ -15,6 +15,7 @@ $tests = [
     'payment_event_ownership_test.php',
     'webhook_amount_validation_test.php',
     'webhook_event_id_test.php',
+    'webhook_lifecycle_event_test.php',
     'payment_transaction_identity_test.php',
     'customer_financial_history_test.php',
     'customer_identity_binding_test.php',
