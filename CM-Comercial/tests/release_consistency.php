8e18521f38775a354540a1be760620d4db9effe0
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 12:52:17 -0300

    test: enforce checkout idempotency coverage in release gate

--- CM-Comercial/tests/release_consistency.php
+++ CM-Comercial/tests/release_consistency.php
@@ -46,6 +46,7 @@ $expected = [
     'payment_status_normalization_runtime_test.php',
     'mercadopago_status_validation_test.php',
     'mercadopago_payment_input_validation_test.php',
+    'checkout_idempotency_scope_test.php',
     'configuration_surface_test.php',
 ];
 
