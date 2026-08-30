0d0967e529dd41fee504374453406a360d003cf7
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 12:33:38 -0300

    test: enforce stock payment policy in release consistency

--- CM-Comercial/tests/release_consistency.php
+++ CM-Comercial/tests/release_consistency.php
@@ -41,6 +41,7 @@ $expected = [
     'stock_payment_idempotency_test.php',
     'stock_payment_bridge_test.php',
     'stock_reconciliation_test.php',
+    'stock_payment_policy_validation_test.php',
     'configuration_surface_test.php',
 ];
 
