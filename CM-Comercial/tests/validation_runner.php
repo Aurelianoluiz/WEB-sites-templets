8f1d48e871a1895348b37e11ef3879aa83bb212c
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 12:40:02 -0300

    test: add Mercado Pago status validation to deterministic runner

--- CM-Comercial/tests/validation_runner.php
+++ CM-Comercial/tests/validation_runner.php
@@ -40,6 +40,7 @@ $tests = [
     'stock_payment_policy_validation_test.php',
     'payment_status_normalization_test.php',
     'payment_status_normalization_runtime_test.php',
+    'mercadopago_status_validation_test.php',
     'configuration_surface_test.php',
 ];
 
