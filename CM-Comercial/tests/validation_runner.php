c1ad3a68e54c80ad493c17701aaebec1acab3f04
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 12:37:05 -0300

    test: register payment status runtime regression

--- CM-Comercial/tests/validation_runner.php
+++ CM-Comercial/tests/validation_runner.php
@@ -39,6 +39,7 @@ $tests = [
     'stock_reconciliation_test.php',
     'stock_payment_policy_validation_test.php',
     'payment_status_normalization_test.php',
+    'payment_status_normalization_runtime_test.php',
     'configuration_surface_test.php',
 ];
 
