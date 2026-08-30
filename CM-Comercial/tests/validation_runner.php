2dc5e1c67c94a3531526f54467735bc76a34c71c
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 12:43:26 -0300

    test: cover Mercado Pago payment input validation

--- CM-Comercial/tests/validation_runner.php
+++ CM-Comercial/tests/validation_runner.php
@@ -41,6 +41,7 @@ $tests = [
     'payment_status_normalization_test.php',
     'payment_status_normalization_runtime_test.php',
     'mercadopago_status_validation_test.php',
+    'mercadopago_payment_input_validation_test.php',
     'configuration_surface_test.php',
 ];
 
