1081fe29985491ac69c3452a2f3f0ba6dd851455
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-30 20:51:36 -0300

    test: make security audit string checks deterministic

--- CM-Comercial/tests/security_audit.php
+++ CM-Comercial/tests/security_audit.php
@@ -10,6 +10,7 @@ $config = $read(__DIR__ . '/../config.php');
 $logout = $read(__DIR__ . '/../logout.php');
 
 $literalUserId = '$_SESSION[\'user\'][\'id\']';
+$logoutPostGuard = 'if ($_SERVER[\'REQUEST_METHOD\'] === \'POST\')';
 $checks = [
     'php_strict_types' => str_contains($financial, 'declare(strict_types=1);'),
     'customer_identity_from_authenticated_session' => str_contains($customer, $literalUserId),
@@ -22,7 +23,7 @@ $checks = [
     'checkout_no_raw_gateway_return' => str_contains($checkout, "'gateway_data' => \$safeGateway"),
     'webhook_present' => file_exists(__DIR__ . '/../webhooks/webhook_handler.php'),
     'htaccess_present' => file_exists(__DIR__ . '/../.htaccess'),
-    'logout_post_guard' => file_exists(__DIR__ . '/../logout.php') && str_contains($logout, "$_SERVER['REQUEST_METHOD'] === 'POST'"),
+    'logout_post_guard' => file_exists(__DIR__ . '/../logout.php') && str_contains($logout, $logoutPostGuard),
     'logout_csrf' => str_contains($logout, 'require_csrf();'),
     'logout_destroy' => str_contains($logout, 'session_destroy();'),
     'strict_session_mode' => str_contains($config, "ini_set('session.use_strict_mode', '1')"),
